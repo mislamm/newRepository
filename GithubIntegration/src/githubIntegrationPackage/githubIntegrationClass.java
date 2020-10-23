1c4b350db52c41b7fa499f7e7352b2687f8847db
muhammad.islam <muhammad.islam@LHRLT-6484.Systemsltd.local>
2020-10-23 13:35:59 +0500

    First Commit

---
+++ GithubIntegration/src/githubIntegrationPackage/githubIntegrationClass.java
@@ -0,0 +1,11 @@
+package githubIntegrationPackage;
+
+public class githubIntegrationClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("New Project created to revise the Github and Jenkins");
+	}
+
+}
